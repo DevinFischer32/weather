fccd6001219bba9dc5e2976e5860d6f3eb2d6fea
Devin Fischer <devinfischer32@gmail.com>
2022-02-01 18:36:12 -0800

    initial comment

---
+++ src/Components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder="City/Location" />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+}
